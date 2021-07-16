349c0a5422f4ca2594ff890b6c7225a9ad1ba82a
edsont8 <edsont8@hotmail.com>
2021-07-16 11:26:26 -0300

    Comunicação com o back-end

--- src/app/conta/services/conta.service.ts
+++ src/app/conta/services/conta.service.ts
@@ -1,13 +1,23 @@
+import { Observable } from 'rxjs';
 import { Usuario } from './../models/usuario';
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, map } from "rxjs/operators";
+import { BaseService } from 'src/app/services/base.service';
 
 @Injectable()
-export class ContaService {
-  constructor(private http: HttpClient) {}
+export class ContaService extends BaseService {
 
-  registrarUsuario(usuario: Usuario) {
+  constructor(private http: HttpClient) { super(); }
 
+  registrarUsuario(usuario: Usuario): Observable<Usuario> {
+    let response = this.http
+    .post(this.UrlServiceV1 + 'nova-conta', usuario, this.ObterHeaderJson())
+    .pipe(
+      map(this.extractData),
+      catchError(this.serviceError));
+
+    return response;
   }
 
   login(usuario: Usuario) {
