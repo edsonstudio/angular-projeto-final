43cfb23e4348d36110bdefea5d01fc9a6c287f7c
edsont8 <edsont8@hotmail.com>
2021-07-17 11:57:08 -0300

    Formulario de login

--- src/app/conta/login/login.component.ts
+++ src/app/conta/login/login.component.ts
@@ -1,14 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { ContaService } from './../services/conta.service';
+import { Usuario } from './../models/usuario';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import { DisplayMessage, GenericValidator, ValidationMessages } from '../../utils/generic-form-validation';
+import { CustomValidators } from 'ng2-validation';
+import { Observable, fromEvent, merge } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
 
-  ngOnInit(): void {
+  loginForm: FormGroup;
+  usuario: Usuario;
+
+  errors: any[] = [];
+  validationMessages: ValidationMessages;
+  genericValidator: GenericValidator;
+  displayMessage: DisplayMessage = {};
+
+  constructor(private fb: FormBuilder,
+    private contaService: ContaService,
+    private router: Router,
+    private toastr: ToastrService) {
+
+    this.validationMessages = {
+      email: {
+        required: 'Informe o e-mail',
+        email: 'Email inválido'
+      },
+      password: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      }
+    };
+
+    this.genericValidator = new GenericValidator(this.validationMessages);
   }
 
+  ngOnInit(): void {
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, CustomValidators.rangeLength([6, 15])]]
+    });
+  }
+
+  ngAfterViewInit(): void {
+    let controlBlurs: Observable<any>[] = this.formInputElements
+      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+    merge(...controlBlurs).subscribe(() => {
+      this.displayMessage = this.genericValidator.processarMensagens(this.loginForm);
+    });
+  }
+
+  login() {
+    if (this.loginForm.dirty && this.loginForm.valid) {
+
+      this.usuario = Object.assign({}, this.usuario, this.loginForm.value);
+
+      this.contaService.login(this.usuario).subscribe(
+        sucesso => { this.processarSucesso(sucesso) },
+        falha => { this.processarFalha(falha) }
+      );
+    }
+  }
+  processarSucesso(response: any) {
+    this.loginForm.reset();
+    this.errors = [];
+    this.contaService.LocalStorage.salvarDadosLocaisUsuario(response);
+
+    let toastr = this.toastr.success('Login efetuado com Sucesso.', 'Seja bem vindo :D');
+    if(toastr) {
+      toastr.onHidden.subscribe(() => {
+        this.router.navigate(['/home']);
+      });
+    }
+  }
+
+  processarFalha(fail: any) {
+    this.errors = fail.error.errors;
+    this.toastr.error('Ocorreu um erro !!!', 'Eita :O');
+  }
 }
