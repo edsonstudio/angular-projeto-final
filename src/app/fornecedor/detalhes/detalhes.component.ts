55a5cc24544520e8f5bb7f5e13a762a616c06f67
edsont8 <edsont8@hotmail.com>
2021-07-18 17:19:33 -0300

    Exibição de mapas

--- src/app/fornecedor/detalhes/detalhes.component.ts
+++ src/app/fornecedor/detalhes/detalhes.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Fornecedor } from '../models/fornecedor';
 
 import { ActivatedRoute } from '@angular/router';
-import { FornecedorService } from '../services/fornecedor.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detalhes',
@@ -11,10 +11,20 @@ import { FornecedorService } from '../services/fornecedor.service';
 export class DetalhesComponent {
 
   fornecedor: Fornecedor = new Fornecedor();
+  enderecoMap;
 
   constructor(
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute, private sanitizer: DomSanitizer) {
 
       this.fornecedor = this.route.snapshot.data['fornecedor'];
+      this.enderecoMap = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.google.com/maps/embed/v1/place?q=" + this.EnderecoCompleto() + "&key=AIzaSyAP0WKpL7uTRHGKWyakgQXbW6FUhrrA5pE");
+  }
+
+  public EnderecoCompleto(): string {
+    return this.fornecedor.endereco.logradouro + ", "
+    + this.fornecedor.endereco.numero + " - "
+    + this.fornecedor.endereco.bairro + ", "
+    + this.fornecedor.endereco.cidade + " - "
+    + this.fornecedor.endereco.estado;
   }
 }
