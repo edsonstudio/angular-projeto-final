ad14dce53a2ffb6ed45ae635a50e5afad1760b35
edsont8 <edsont8@hotmail.com>
2021-07-19 08:37:12 -0300

    Clean Code: Refatorando serviços de guarda

--- src/app/fornecedor/services/fornecedor.guard.ts
+++ src/app/fornecedor/services/fornecedor.guard.ts
@@ -1,15 +1,13 @@
-import { LocalStorageUtils } from './../../utils/localstorage';
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, Router } from "@angular/router";
+
 import { NovoComponent } from '../novo/novo.component';
+import { BaseGuard } from 'src/app/services/base.guard';
 
 @Injectable()
-export class FornecedorGuard implements CanActivate, CanDeactivate<NovoComponent> {
+export class FornecedorGuard extends BaseGuard implements CanActivate, CanDeactivate<NovoComponent> {
 
-  localStorageUtils = new LocalStorageUtils();
-
-  constructor(private router: Router) { }
+  constructor(protected router: Router) { super(router); }
 
   canDeactivate(component: NovoComponent) {
     if (component.mudancasNaoSalvas) {
@@ -18,41 +16,7 @@ export class FornecedorGuard implements CanActivate, CanDeactivate<NovoComponent
     return true
   }
 
-  canActivate(routeAc: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
-    if (!this.localStorageUtils.obterTokenUsuario()) {
-      this.router.navigate(['/conta/login'], { queryParams: { returnUrl: this.router.url }});
-    }
-
-    let user = this.localStorageUtils.obterUsuario();
-    let claim: any = routeAc.data[0];
-
-    if (claim !== undefined) {
-      let claim = routeAc.data[0]['claim'];
-
-      if (claim) {
-        if (!user.claims) {
-          this.navegarAcessoNegado();
-        }
-
-        let userClaims = user.claims.find(x => x.type === claim.nome);
-
-        if (!userClaims) {
-          this.navegarAcessoNegado();
-        }
-
-        let valoresClaim = userClaims.value as string; //cast para string
-
-        if (!valoresClaim.includes(claim.valor)) {
-          this.navegarAcessoNegado();
-        }
-      }
-    }
-
-    return true;
-  }
-
-  navegarAcessoNegado() {
-    this.router.navigate(['/acesso-negado']);
+  canActivate(routeAc: ActivatedRouteSnapshot) {
+    return super.validarClaims(routeAc);
   }
 }
