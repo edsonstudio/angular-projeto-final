b53fcaf329dc93f9c4509fed851b67b93d164252
edsont8 <edsont8@hotmail.com>
2021-09-25 14:24:08 -0300

    novo backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlV1: 'https://back-angular-final.azurewebsites.net/api/v1/',
-  imagensUrl: 'https://back-angular-final.azurewebsites.net/'
+  apiUrlV1: 'https://dev-io-proj-final.azurewebsites.net/api/v1/',
+  imagensUrl: 'https://dev-io-proj-final.azurewebsites.net/'
 };
